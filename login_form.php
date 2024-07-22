e611dfb9e0d0a9083b6ff70ae60072b7ed252f9b
jayric <48792831+jayriccaintoy5@users.noreply.github.com>
2024-07-22 16:11:22 +0800

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
 <?php
 
 @include 'config.php';
@@ -40,9 +43,7 @@ if(isset($_POST['submit'])){
 };
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
+
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
